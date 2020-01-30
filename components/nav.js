dabdcba9dce02eaf5293761e0d4c83cfcddba60c
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-01-30 13:04:36 +0400

    commit 4

--- components/nav.js
+++ components/nav.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
+  { href: "https://zeit.co/now", label: "ZEIT" },
+  { href: "https://github.com/zeit/next.js", label: "GitHub" }
 ].map(link => ({
   ...link,
-  key: `nav-link-${link.href}-${link.label}`,
-}))
+  key: `nav-link-${link.href}-${link.label}`
+}));
 
 const Nav = () => (
   <nav>
@@ -15,6 +15,9 @@ const Nav = () => (
       <li>
         <Link href="/">
           <a>Home</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
         </Link>
       </li>
       {links.map(({ key, href, label }) => (
@@ -51,6 +54,6 @@ const Nav = () => (
       }
     `}</style>
   </nav>
-)
+);
 
-export default Nav
+export default Nav;
