9ded9be33d3af411fea4f4e89120292fd5e7bd3b
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-01-30 14:29:37 +0400

    commit 6

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,19 +1,25 @@
-// Short Way
-export default (req, res) => {
-  console.log("hello");
-  const name = "scott";
-  res.status(200).json({
-    test: "Hello level Up"
-  });
+import { ApolloServer, gql } from "apollo-server-micro";
+
+const typeDefs = gql`
+  type Query {
+    sayHello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHello: () => {
+      return "Hello level Up";
+    }
+  }
 };
 
-// Long Way
-// export default (req, res) => {
-//   res.setHeader("Content-Type", "application/json");
-//   res.statusCode = 200;
-//   res.end(
-//     JSON.stringify({
-//       test: "Hello level Up"
-//     })
-//   );
-// };
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+export default apolloServer.createHandler({ path: "/api/graphql" });
